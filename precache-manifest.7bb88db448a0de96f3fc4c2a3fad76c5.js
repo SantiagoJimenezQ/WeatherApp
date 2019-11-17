1e249a6ee61f89a0f0722b9daa7420900d9f6171
Santiago <santyraiman08@hotmail.com>
2019-11-17 18:21:56 +0100

    Updates

---
+++ precache-manifest.7bb88db448a0de96f3fc4c2a3fad76c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc71d013d0abcda493a8c9236e8cb80c",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "51aaca6092a4392fd6de",
+    "url": "/WeatherApp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b9740adecdf623541772",
+    "url": "/WeatherApp/static/css/main.10847205.chunk.css"
+  },
+  {
+    "revision": "51aaca6092a4392fd6de",
+    "url": "/WeatherApp/static/js/2.bd5275d2.chunk.js"
+  },
+  {
+    "revision": "b9740adecdf623541772",
+    "url": "/WeatherApp/static/js/main.80260524.chunk.js"
+  },
+  {
+    "revision": "50a7104050884bb5ef16",
+    "url": "/WeatherApp/static/js/runtime-main.a174ed14.js"
+  },
+  {
+    "revision": "6833831c33f18df85e3531ea59cbb1c0",
+    "url": "/WeatherApp/static/media/bg.6833831c.jpg"
+  }
+]);
